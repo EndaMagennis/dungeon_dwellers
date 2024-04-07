6aa45af8c72c9e38c61592b64da8ecb495e9d513
Enda <endamagennis@gmail.com>
2024-04-07 14:53:41 +0100

    Use environment variables for secret key

--- dungeon_dwellers/settings.py
+++ dungeon_dwellers/settings.py
@@ -21,7 +21,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--7+j5&4#x7s5xk(50cy=yfp46g74b1oyoyf%-p7f!5vzdxb&le'
+if os.path.exists('env.py'):
+    import env
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
