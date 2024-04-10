4b14df34f50530ac33a6be7d2458e53e152e83b3
Enda Magennis <endamagennis@gmail.com>
2024-04-10 22:23:23 +0000

    Change database

--- dungeon_dwellers/settings.py
+++ dungeon_dwellers/settings.py
@@ -130,17 +130,17 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
-
 # DATABASES = {
-#     'default':
-#     dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#     }
 # }
+
+DATABASES = {
+    'default':
+    dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
